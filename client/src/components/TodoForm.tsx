ba16d7d9ef5984363da23b24e9e513a4a5b327a4
SantinoGiordano <santinogiordano13@gmail.com>
2025-02-13 12:58:22 -0500

    big stylking changes everywhere

--- client/src/components/TodoForm.tsx
+++ client/src/components/TodoForm.tsx
@@ -65,9 +65,9 @@
 // };
 // export default TodoForm;
 
-import { Button, Flex, Input, Spinner } from "@chakra-ui/react";
 import { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
+import { ImSpinner8 } from "react-icons/im";
 
 const TodoForm = () => {
 	const [newTodo, setNewTodo] = useState("");
@@ -77,27 +77,32 @@ const TodoForm = () => {
 		e.preventDefault();
 		alert("Todo added!");
 	};
-    
+
 	return (
-		<form onSubmit={createTodo}>
-			<Flex gap={2}>
-				<Input
-					type='text'
-					value={newTodo}
-					onChange={(e) => setNewTodo(e.target.value)}
-					ref={(input) => input && input.focus()}
-				/>
-				<Button
-					mx={2}
-					type='submit'
-					_active={{
-						transform: "scale(.97)",
-					}}
-				>
-					{isPending ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
-				</Button>
-			</Flex>
-		</form>
+		<div className="text-center">
+			<form onSubmit={createTodo}>
+				<div className="flex gap-2">
+					<input
+						type="text"
+						value={newTodo}
+						onChange={(e) => setNewTodo(e.target.value)}
+						className="flex-1 p-2 border border-gray-300 rounded-lg ring-2 ring-blue-500 focus:outline-none focus:border-blue-500"
+						autoFocus
+					/>
+					<button
+						type="submit"
+						className="p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 active:scale-95 transition-transform"
+					>
+						{isPending ? (
+							<ImSpinner8 className="animate-spin" />
+						) : (
+							<IoMdAdd size={20} />
+						)}
+					</button>
+				</div>
+			</form>
+		</div>
 	);
 };
+
 export default TodoForm;
