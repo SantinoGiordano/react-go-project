b1e4140835391aa016d35540dd7c193f0530be58
SantinoGiordano <santinogiordano13@gmail.com>
2025-02-13 11:27:57 -0500

    App moved into provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,10 +5,12 @@ import App from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
 
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
   <Provider>
-    <App />
+      <App />
   </Provider>
   
   </StrictMode>,
