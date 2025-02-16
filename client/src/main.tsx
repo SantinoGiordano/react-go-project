4319260e3d7e615f380afe1f16a4b31a237923fd
SantinoGiordano <santinogiordano13@gmail.com>
2025-02-16 13:07:14 -0500

    created small bit of code where we import and initilize tanstack

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from './components/ui/provider.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "./components/ui/provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
-
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-
-  <Provider>
-      <App />
-  </Provider>
-  
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+      </Provider>
+    </QueryClientProvider>
+  </StrictMode>
+);
